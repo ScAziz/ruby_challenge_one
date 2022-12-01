6d17fbb395bee7ac775878ea1a79e7aecdc0400a
steveaziz <stephenaziz.sa@gmail.com>
2022-12-01 13:56:19 +0000

    provided solution

--- lib/password_validator.rb
+++ lib/password_validator.rb
@@ -2,3 +2,6 @@
 # - 8 or more characters
 # - include at least one of these characters: !, @, $, %, &
 
+def valid?(password)
+    return password.length >= 8 && password[/[!|@|$|%|&]/] ? true : false
+end 
